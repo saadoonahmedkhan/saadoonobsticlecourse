0ac6a2037263128aa251d37fde3ec6f33e040d28
shaheerahmedkhan11 <shaheerahmedkhan11@gmail.com>
2024-08-11 16:12:06 +0500

    object is moving

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    float yvalue = 0;
+    [SerializeField]
+    float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xvalue = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;
+        float zvalue = Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed;
+        transform.Translate(xvalue, yvalue, zvalue);
+    }
+}
